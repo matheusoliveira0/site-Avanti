26ed67df30d518fa6ca4ddf201ff3ec25c201415
Matheus Oliveira <teumatheus.73@gmail.com>
2025-04-16 20:50:44 -0300

    mobile first

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,31 @@
-const accordions = document.querySelectorAll(".sobre-avanti");
+// codigo da barra de pesquisa
+
+// // meu cod
+const searchInput = document.getElementById("input-search");
+const searchBtn = document.getElementById("btn-search");
+const searchResult = document.getElementById("search-result");
+
+searchResult.style.display = "none";
+
+searchBtn.addEventListener("click", () => {
+  const searchText = searchInput.value.trim();
+  searchResult.style.display = "block";
+  if (searchText !== "") {
+    searchResult.innerHTML = `<p>Você buscou por: ${searchText}.</p>`;
+  } else {
+    searchResult.innerHTML = "Por favor, digite algo para buscar.";
+  }
+});
+
+searchInput.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    searchBtn.click();
+  }
+});
+
+// codigo dos accordions
+
+const accordions = document.querySelectorAll(".accordion");
 
 accordions.forEach((accordion) => {
   accordion.addEventListener("click", () => {
