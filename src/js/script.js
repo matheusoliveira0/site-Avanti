847af5b79135c1975e475fc18224a54f98347e9a
Matheus Oliveira <teumatheus.73@gmail.com>
2025-04-17 17:59:17 -0300

    mobile first finalizado

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,5 @@
 // codigo da barra de pesquisa
 
-// // meu cod
 const searchInput = document.getElementById("input-search");
 const searchBtn = document.getElementById("btn-search");
 const searchResult = document.getElementById("search-result");
@@ -11,7 +10,7 @@ searchBtn.addEventListener("click", () => {
   const searchText = searchInput.value.trim();
   searchResult.style.display = "block";
   if (searchText !== "") {
-    searchResult.innerHTML = `<p>Você buscou por: ${searchText}.</p>`;
+    searchResult.innerHTML = `Você buscou por: <strong>${searchText}</strong>`;
   } else {
     searchResult.innerHTML = "Por favor, digite algo para buscar.";
   }
@@ -21,6 +20,39 @@ searchInput.addEventListener("keypress", (e) => {
   if (e.key === "Enter") {
     searchBtn.click();
   }
+});
+
+// codigo dos sliders
+
+new Swiper(".slide1", {
+  loop: true,
+  // spaceBetween:10,
+
+  // Pagination bullets
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".arrow-left",
+    prevEl: ".arrow-right",
+  },
+
+  // Responsive breakpoints
+  breakpoints: {
+    0: {
+      slidesPerView: 2,
+    },
+    640: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 5,
+    },
+  },
 });
 
 // codigo dos accordions
